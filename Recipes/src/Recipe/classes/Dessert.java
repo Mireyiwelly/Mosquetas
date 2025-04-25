687bcdc8859c668db79307cd5b3dff639b7451f1
hjga24 <hjga24@gmail.com>
2025-04-25 12:23:39 +0200

    Create Dessert class

---
+++ Recipes/src/Recipe/classes/Dessert.java
@@ -0,0 +1,67 @@
+package Recipe.classes;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Dessert extends Recipe
+{
+    private boolean baked;
+    private int restingTime;
+    private int bakingTime;
+    private int servingTemperature;
+    private DessertType dessertType;
+
+    public Dessert(String name, int numDiners, String preparation, List<Ingredient> ingredients, float calories,
+                   char difficultyLevel, SpecialDiets specialDiet, LocalDate publicationDate, int preparationTime,
+                   User author, boolean baked, int restingTime, int bakingTime, int servingTemperature,
+                   DessertType dessertType)
+    {
+        super(name, numDiners, preparation, ingredients, calories, difficultyLevel,
+                specialDiet, publicationDate, preparationTime, author);
+        this.baked = baked;
+        this.restingTime = restingTime;
+        this.bakingTime = bakingTime;
+        this.servingTemperature = servingTemperature;
+        this.dessertType = dessertType;
+    }
+
+    public boolean isBaked() {
+        return baked;
+    }
+
+    public void setBaked(boolean baked) {
+        this.baked = baked;
+    }
+
+    public int getRestingTime() {
+        return restingTime;
+    }
+
+    public void setRestingTime(int restingTime) {
+        this.restingTime = restingTime;
+    }
+
+    public int getBakingTime() {
+        return bakingTime;
+    }
+
+    public void setBakingTime(int bakingTime) {
+        this.bakingTime = bakingTime;
+    }
+
+    public int getServingTemperature() {
+        return servingTemperature;
+    }
+
+    public void setServingTemperature(int servingTemperature) {
+        this.servingTemperature = servingTemperature;
+    }
+
+    public DessertType getDessertType() {
+        return dessertType;
+    }
+
+    public void setDessertType(DessertType dessertType) {
+        this.dessertType = dessertType;
+    }
+}
---
+++ Recipes/src/Recipe/classes/DessertType.java
@@ -0,0 +1,5 @@
+package Recipe.classes;
+
+public enum DessertType {
+    //no se que tipo de postre hay
+}
