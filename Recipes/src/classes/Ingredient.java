e7f4542a269c36846a98eb4315e6cf9c7789cb6d
hjga24 <hjga24@gmail.com>
2025-04-25 10:57:12 +0200

    Create Recipe and Ingredient class

---
+++ Recipes/src/classes/Ingredient.java
@@ -0,0 +1,38 @@
+package classes;
+
+public class Ingredient
+{
+    private String name;
+    private int number;
+    private String unit;
+
+    public Ingredient(String name, int number, String unit) {
+        this.name = name;
+        this.number = number;
+        this.unit = unit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+}
---
+++ Recipes/src/classes/Recipe.java
@@ -0,0 +1,12 @@
+package classes;
+
+import java.util.List;
+
+abstract public class Recipe
+{
+    protected String name;
+    protected int numDiners;
+    protected String preparation;
+    protected List<Ingredient> ingredients;
+
+}
