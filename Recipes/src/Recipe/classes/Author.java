95765cf071b62ee78179499ccc07485436d0031f
David L. <david16lp2002@gmail.com>
2025-04-25 11:19:08 +0200

    Added comments to the User and Author classes

--- Recipes/src/Recipe/classes/Author.java
+++ Recipes/src/Recipe/classes/Author.java
@@ -1,10 +1,44 @@
 package Recipe.classes;
-import Recipe.classes.User;
+import Recipe.classes.*;
+import java.util.List;
 
+/**
+ * Represents an author in the recipe management system.
+ * Inherits from the User class and adds a password attribute.
+ */
 public class Author extends User
 {
-    public Author(String name, String password, int numRecipes, List<Recipe> createdRecipes)
+    protected String password;
+
+    /**
+     * Constructor for Author class.
+     * @param name the name of the author
+     * @param numRecipes the number of recipes created by the author
+     * @param createdRecipes the list of recipes created by the author
+     * @param password the password of the author
+     */
+    public Author(String name, int numRecipes, List<Recipe> createdRecipes,
+                  String password)
     {
-        super(name, password, numRecipes, createdRecipes);
+        super(name, numRecipes, createdRecipes);
+        this.password = password;
+    }
+
+    /**
+     * Gets the password of the author.
+     * @return the password of the author
+     */
+    public String getPassword()
+    {
+        return password;
+    }
+
+    /**
+     * Sets the password of the author.
+     * @param password the new password of the author
+     */
+    public void setPassword(String password)
+    {
+        this.password = password;
     }
 }
--- Recipes/src/Recipe/classes/User.java
+++ Recipes/src/Recipe/classes/User.java
@@ -1,12 +1,23 @@
 package Recipe.classes;
 
+import java.util.List;
+
+/**
+ * Abstract class User that represents a user in the recipe management system.
+ * It contains common attributes and methods for all types of users.
+ */
 abstract public class User
 {
     protected String name;
-    protected String password;
     protected int numRecipes;
     protected List<Recipe> createdRecipes;
 
+    /**
+     * Constructor for User class.
+     * @param name the name of the user
+     * @param numRecipes the number of recipes created by the user
+     * @param createdRecipes the list of recipes created by the user
+     */
     public User(String name, int numRecipes, List<Recipe> createdRecipes)
     {
         this.name = name;
@@ -14,43 +25,55 @@ abstract public class User
         this.createdRecipes = createdRecipes;
     }
 
-
-
+    /**
+     * Gets the name of the user.
+     * @return the name of the user
+     */
     public String getName()
     {
         return name;
     }
 
+    /**
+     * Sets the name of the user.
+     * @param name the new name of the user
+     */
     public void setName(String name)
     {
         this.name = name;
     }
 
-    public String getPassword()
-    {
-        return password;
-    }
-
-    public void setPassword(String password)
-    {
-        this.password = password;
-    }
-
+    /**
+     * Gets the number of recipes created by the user.
+     * @return the number of recipes
+     */
     public int getNumRecipes()
     {
         return numRecipes;
     }
 
+    /**
+     * Sets the number of recipes created by the user.
+     * @param numRecipes the new number of recipes
+     */
     public void setNumRecipes(int numRecipes)
     {
         this.numRecipes = numRecipes;
     }
 
+    /**
+     * Gets the list of recipes created by the user.
+     * @return the list of recipes
+     */
     public List<Recipe> getCreatedRecipes()
     {
         return createdRecipes;
     }
 
+    /**
+     * Sets the list of recipes created by the user.
+     * @param createdRecipes the new list of recipes
+     */
     public void setCreatedRecipes(List<Recipe> createdRecipes)
     {
         this.createdRecipes = createdRecipes;
