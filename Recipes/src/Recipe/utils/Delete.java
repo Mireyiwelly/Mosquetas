55af6fb29d558a6aa21468dc681a7a629b7086a7
hjga24 <hjga24@gmail.com>
2025-04-27 23:27:37 +0200

    Create delete by name method

--- Recipes/src/Recipe/utils/Delete.java
+++ Recipes/src/Recipe/utils/Delete.java
@@ -2,6 +2,7 @@ package Recipe.utils;
 
 import Recipe.classes.Recipe;
 
+import java.util.Iterator;
 import java.util.List;
 
 public class Delete
@@ -15,4 +16,24 @@ public class Delete
     {
 
     }
+
+    public static void DeleteByName(List<Recipe> recipes, String nameToDelete, String nameUser)
+    {
+        //Iterator te permite eliminar elementos mientras la recorre
+        Iterator<Recipe> deletedRecipes = recipes.iterator();
+        //este while te elimina todos los nombres que coincidan (No deberia de haber dos recetas con el mismo nombre)
+        while (deletedRecipes.hasNext())
+        {
+            Recipe recipe = deletedRecipes.next();
+            //no sé si está condición está bien, busca el nombre de la receta, que el autor no sea nul(creo que no hace falta) y el nombre del autor de la receta(un poco invent)
+            if (recipe.getName().equalsIgnoreCase(nameToDelete) &&
+                    recipe.getAuthor() != null &&
+                    recipe.getAuthor().getName().equalsIgnoreCase(nameUser))
+            {
+                deletedRecipes.remove();
+            }
+        }
+    }
+
+
 }
