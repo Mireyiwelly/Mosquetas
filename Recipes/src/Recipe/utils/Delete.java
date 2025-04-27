f2a6e3a0d9683f13cdf331a558d97f79d0ed6827
David L. <david16lp2002@gmail.com>
2025-04-27 22:02:42 +0200

    Created Delete Class
    Delete Class with all options to delete recipes and users

--- Recipes/src/Recipe/utils/Delete.java
+++ Recipes/src/Recipe/utils/Delete.java
@@ -6,6 +6,11 @@ import java.util.List;
 
 public class Delete
 {
+    /**
+     * Deletes a recipe from the list of recipes based on the dish type.
+     *
+     * @param recipes the list of recipes
+     */
     public static void DeleteRecipeByDishType(List<Recipe> recipes)
     {
 
