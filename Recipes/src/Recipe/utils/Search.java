fc900c0e3b782fcc2937ab3adcc49b7f27ff06d5
hjga24 <hjga24@gmail.com>
2025-04-27 22:39:56 +0200

    Update Search.java

--- Recipes/src/Recipe/utils/Search.java
+++ Recipes/src/Recipe/utils/Search.java
@@ -165,7 +165,8 @@ public class Search
             }
         }
 
-        if (!found) {
+        if (!found)
+        {
             System.out.println("No recipes found for this preparation time.");
         }
     }
