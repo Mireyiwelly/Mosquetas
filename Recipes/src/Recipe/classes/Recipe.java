bbfcbe371a8c25d5cc39d0e6eb04fd4b284603e8
David L. <david16lp2002@gmail.com>
2025-04-28 09:53:48 +0200

    Fixed toFile Method
    It was repeated

--- Recipes/src/Recipe/classes/Recipe.java
+++ Recipes/src/Recipe/classes/Recipe.java
@@ -133,17 +133,6 @@ abstract public class Recipe
 
     }
 
-    public String toFile() {
-        //Aquí junto todos los elementos de los ingredientes ya que no se puede acceder a la clase directamente como tal
-        StringBuilder ingredientsInfo = new StringBuilder();
-        for (Ingredient ingredient : ingredients) {
-            ingredientsInfo.append(ingredient.getName()).append(";").append(ingredient.getNumber()).append(";")
-                    .append(ingredient.getUnit());
-        }
-        return dishType + ";" + name + ";" + numDiners + ";" + preparation + ";" + ingredientsInfo + ";" + calories +
-                ";" + difficultyLevel + ";" + specialDiet + ";" + publicationDate + ";" + preparationTime + ";" + author;
-    }
-
         @Override
     public String toString()
     {
