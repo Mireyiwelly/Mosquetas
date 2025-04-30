cd7f7c5eb94a6205561944d74e51169a727dc15f
David L. <david16lp2002@gmail.com>
2025-04-30 09:33:20 +0200

    Added Comparable Method to order recipes by name

--- Recipes/src/Recipe/classes/Recipe.java
+++ Recipes/src/Recipe/classes/Recipe.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 
-abstract public class Recipe
+abstract public class Recipe implements Comparable<Recipe>
 {
     protected String name;
     protected int numDiners;
@@ -20,7 +20,8 @@ abstract public class Recipe
 
     public Recipe(String name, int numDiners, String preparation, List<Ingredient> ingredients, float calories,
                   char difficultyLevel, SpecialDiets specialDiet, LocalDate publicationDate,
-                  int preparationTime, User author, char dishType){
+                  int preparationTime, User author, char dishType)
+    {
         this.name = name;
         this.numDiners = numDiners;
         this.preparation = preparation;
@@ -34,91 +35,113 @@ abstract public class Recipe
         this.dishType = dishType;
     }
 
-    public String getName() {
+    public String getName()
+    {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(String name)
+    {
         this.name = name;
     }
 
-    public int getNumDiners() {
+    public int getNumDiners()
+    {
         return numDiners;
     }
 
-    public void setNumDiners(int numDiners) {
+    public void setNumDiners(int numDiners)
+    {
         this.numDiners = numDiners;
     }
 
-    public String getPreparation() {
+    public String getPreparation()
+    {
         return preparation;
     }
 
-    public void setPreparation(String preparation) {
+    public void setPreparation(String preparation)
+    {
         this.preparation = preparation;
     }
 
-    public float getCalories() {
+    public float getCalories()
+    {
         return calories;
     }
 
-    public void setCalories(float calories) {
+    public void setCalories(float calories)
+    {
         this.calories = calories;
     }
 
-    public List<Ingredient> getIngredients() {
+    public List<Ingredient> getIngredients()
+    {
         return ingredients;
     }
 
-    public void setIngredients(List<Ingredient> ingredients) {
+    public void setIngredients(List<Ingredient> ingredients)
+    {
         this.ingredients = ingredients;
     }
 
-    public char getDifficultyLevel() {
+    public char getDifficultyLevel()
+    {
         return difficultyLevel;
     }
 
-    public void setDifficultyLevel(char difficultyLevel) {
+    public void setDifficultyLevel(char difficultyLevel)
+    {
         this.difficultyLevel = difficultyLevel;
     }
 
-    public SpecialDiets getSpecialDiet() {
+    public SpecialDiets getSpecialDiet()
+    {
         return specialDiet;
     }
 
-    public void setSpecialDiet(SpecialDiets specialDiet) {
+    public void setSpecialDiet(SpecialDiets specialDiet)
+    {
         this.specialDiet = specialDiet;
     }
 
-    public LocalDate getPublicationDate() {
+    public LocalDate getPublicationDate()
+    {
         return publicationDate;
     }
 
-    public void setPublicationDate(LocalDate publicationDate) {
+    public void setPublicationDate(LocalDate publicationDate)
+    {
         this.publicationDate = publicationDate;
     }
 
-    public int getPreparationTime() {
+    public int getPreparationTime()
+    {
         return preparationTime;
     }
 
-    public void setPreparationTime(int preparationTime) {
+    public void setPreparationTime(int preparationTime)
+    {
         this.preparationTime = preparationTime;
     }
 
-    public User getAuthor() {
+    public User getAuthor()
+    {
         return author;
     }
 
-    public void setAuthor(User author) {
+    public void setAuthor(User author)
+    {
         this.author = author;
     }
 
 
-    public String toFile() {
+    public String toFile()
+    {
         //Aquí junto todos los elementos de los ingredientes ya que no se puede acceder a la clase directamente como tal
         StringBuilder ingredientsInfo = new StringBuilder();
-        for (Ingredient ingredient : ingredients) {
+        for (Ingredient ingredient : ingredients)
+        {
             ingredientsInfo.append(ingredient.getName()).append(";").append(ingredient.getNumber()).append(";")
                     .append(ingredient.getUnit());
         }
@@ -126,12 +149,19 @@ abstract public class Recipe
                 ";" + difficultyLevel + ";" + specialDiet + ";" + publicationDate + ";" + preparationTime + ";" + author;
     }
 
-    public char getDishType() {
+    public char getDishType()
+    {
         return dishType;
 
     }
 
-        @Override
+    @Override
+    public int compareTo(Recipe other)
+    {
+        return this.name.compareToIgnoreCase(other.name);
+    }
+
+    @Override
     public String toString()
     {
         return "Name: " + name + "\n" +
